4e2a60cf1f82ef313614ec857e30238df90625c8
joaco_broggi74 <playstationgta84@gmail.com>
2022-07-22 18:31:52 -0300

    d

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
-let sobreMiBtn = document.querySelectorAll('.sobreMi')
+let sobreMiBtn = document.querySelectorAll('.sobreMi');
 let proyectosBtn = document.querySelectorAll('.proyectos');
-let conocimientosBtn = document.querySelectorAll('.servicios')
-let contactoBtn = document.querySelectorAll('.contacto')
+let conocimientosBtn = document.querySelectorAll('.servicios');
+let contactoBtn = document.querySelectorAll('.contacto');
+
+let sobreMimobile = document.getElementById('sobreMiMobile')
+let proyectoMobile = document.getElementById('proyectoMobile')
+let serviciosMimobile = document.getElementById('serviciosMiMobile')
+
+
 
 let vh = window.innerHeight * 0.01;
 
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-
+sobreMimobile.addEventListener('click', function () {
+    window.scroll(0,findPos(document.getElementById("sobreMobile")));
+})
+proyectoMobile.addEventListener('click', function () {
+    window.scroll(0,findPos(document.getElementById("proyectoMobil")));
+})
+serviciosMimobile.addEventListener('click', function () {
+    window.scroll(0,findPos(document.getElementById("habil")));
+})
 
 proyectosBtn.addEventListener('click', function () {
     window.scroll(0,findPos(document.getElementById("proyectos")));
