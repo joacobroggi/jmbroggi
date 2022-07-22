d55d4216990e3b06fd42a3f916c01ddf1fb3a73a
joaco_broggi74 <playstationgta84@gmail.com>
2022-07-22 17:52:42 -0300

    portfolio

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-let sobreMiBtn = document.querySelector('.sobreMi')
-let proyectosBtn = document.querySelector('.proyectos');
-let conocimientosBtn = document.querySelector('.servicios')
-let contactoBtn = document.querySelector('.contacto')
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let sobreMiBtn = document.querySelectorAll('.sobreMi')
+let proyectosBtn = document.querySelectorAll('.proyectos');
+let conocimientosBtn = document.querySelectorAll('.servicios')
+let contactoBtn = document.querySelectorAll('.contacto')
+
 let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
+
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 
